1cb5ca23a3f18c9988c9466570d5dc806b4ff288
Kishan Tiwari <kishan98tiwari@gmail.com>
2020-04-15 13:42:14 +0530

    Create Overlapping rectangles.cpp

---
+++ Overlapping rectangles.cpp
@@ -0,0 +1,45 @@
+/*
+  Author - KISHAN TIWARI
+  Dream bigger. Do bigger.
+  Wake up with determination. Go to bed with satisfaction.
+*/
+//Sometimes later becomes never. Do it now.
+#include<bits/stdc++.h>
+using namespace std;
+#define ll           long long
+#define f(i,a,b)     for(ll i=a;i<b;i++)
+#define mod          1000000007
+#define fi           first
+#define se           second
+#define pa           pair <ll,ll>
+#define mp           make_pair
+#define pb           push_back
+#define ve           vector<ll>
+#define vep          vector< pa >
+#define all(a)       a.begin(), a.end()
+#define sz(x)        (ll)x.size()
+#define c(t)         cout<<t<<"\n"
+using namespace std;
+void solve()
+{  ll x1,y1,x2,y2,l1,l2,l3,l4;
+cin>>x1>>y1>>x2>>y2>>l1>>l2>>l3>>l4;
+if(x1<=l3&&l1<=x2&&l4<=y1&&y2<=l2)
+    c(1);
+else
+    c(0);
+}
+int main()
+{
+    ios::sync_with_stdio(true);
+   //freopen ("input.txt","r",stdin);
+   //freopen ("output.txt","w",stdout);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+return 0;
+}
