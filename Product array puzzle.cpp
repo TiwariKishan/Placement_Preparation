a07d5dd6770319c1640fb2ed23fcb3ce1fbc5334
Kishan Tiwari <kishan98tiwari@gmail.com>
2020-05-07 11:45:01 +0530

    Create Product array puzzle.cpp

---
+++ Product array puzzle.cpp
@@ -0,0 +1,55 @@
+/*
+  Author - KISHAN TIWARI
+  Dream bigger. Do bigger.
+  Wake up with determination. Go to bed with satisfaction.
+*/
+//Sometimes later becomes never. Do it now.
+#include<bits/stdc++.h>
+using namespace std;
+#define ll           long long
+#define f(i,a,b)     for(ll i=a;i<b;i++)
+#define mod          1000000007
+#define fi           first
+#define se           second
+#define pa           pair <ll,ll>
+#define mp           make_pair
+#define pb           push_back
+#define ve           vector<ll>
+#define vep          vector< pa >
+#define all(a)       a.begin(), a.end()
+#define sz(x)        (ll)x.size()
+#define c(t)         cout<<t<<"\n"
+using namespace std;
+void solve()
+{
+ll n;
+cin>>n;
+ll a[n];
+f(i,0,n)
+cin>>a[i];
+ll pr[n+2],su[n+2];
+pr[0]=1;
+su[n+1]=1;
+f(i,1,n+1)
+pr[i]=pr[i-1]*a[i-1];
+for(ll i=n;i>=1;i--)
+su[i]=su[i+1]*a[i-1];
+f(i,1,n+1)
+cout<<pr[i-1]*su[i+1]<<" ";
+cout<<"\n";
+}
+int main()
+{
+    ios::sync_with_stdio(true);
+   //freopen ("input.txt","r",stdin);
+   //freopen ("output.txt","w",stdout);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+return 0;
+}
