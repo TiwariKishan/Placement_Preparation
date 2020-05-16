59f640342c90252f9e25d3b3a12bef1b82fdcf7d
Kishan Tiwari <kishan98tiwari@gmail.com>
2020-05-16 13:57:41 +0530

    O(logN)
    Repetation of element

---
+++ Find Minimum in Rotated Sorted Array II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        {
+        int l=0,r=nums.size()-1,m;
+while(l<r)
+{
+    m=l+(r-l)/2;
+    if(nums[m]==nums[r])
+        r--;
+    else if(nums[m]>nums[r])
+        l=m+1;
+    else
+        r=m;
+}
+return nums[r];
+    }
+    }
+};
