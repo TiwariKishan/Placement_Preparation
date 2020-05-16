d54d220b92306cb4060ce5585f9436275b13631b
Kishan Tiwari <kishan98tiwari@gmail.com>
2020-05-16 13:45:19 +0530

    Create Minimum element in a sorted and rotated array.cpp

---
+++ Minimum element in a sorted and rotated array.cpp
@@ -0,0 +1,55 @@
+/*
+  Author - KISHAN TIWARI
+  Dream bigger. Do bigger.
+  Wake up with determination. Go to bed with satisfaction.
+*/
+//Sometimes later becomes never. Do it now.
+#include<bits/stdc++.h>
+using namespace std;
+#define ll           long long
+#define f(i,a,b)     for(ll i=a;i<b;i++)
+#define mod          1000000007
+#define fi           first
+#define se           second
+#define pa           pair <ll,ll>
+#define mp           make_pair
+#define pb           push_back
+#define ve           vector<ll>
+#define vep          vector< pa >
+#define all(a)       a.begin(), a.end()
+#define sz(x)        (ll)x.size()
+#define c(t)         cout<<t<<"\n"
+using namespace std;
+void solve()
+{
+ll n;
+cin>>n;
+ll a[n];
+f(i,0,n)
+cin>>a[i];
+ll l=0,r=n-1,m;
+while(l<r)
+{
+    m=l+(r-l)/2;
+    if(a[m]>a[r])
+        l=m+1;
+    else
+        r=m;
+}
+c(a[r]);
+}
+int main()
+{
+    ios::sync_with_stdio(true);
+   //freopen ("input.txt","r",stdin);
+   //freopen ("output.txt","w",stdout);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+return 0;
+}
