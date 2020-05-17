0f6991c4c5c0ef8b354f448d2d62c12a9634c721
Kishan Tiwari <kishan98tiwari@gmail.com>
2020-05-17 06:05:31 +0530

    t.c=O(log N)

--- Peak_finding.cpp
+++ Peak_finding.cpp
@@ -1,15 +1,18 @@
-int pr(int a[],int l,int r,int n)
-{
-    int mid=l+(l-r)/2;
-    if((mid==0||a[mid-1]<a[mid])&&(mid==n-1||a[mid]>a[mid+1]))
-    return mid;
-    else if(mid!=0&&a[mid-1]>a[mid])
-    return pr(a,l,mid-1,n);
-    else
-    return pr(a,mid+1,r,n);
-}
+int peak(int nums[],int n)
+{ //int n=nums.size();
+        int l=0,r=n-1,m;
+        while(l<r)
+        {
+            m=l+(r-l)/2;
+            if(nums[m]<nums[m+1])
+                l=m+1;
+            else
+                r=m;
+        }
+        return r;
+    }
 int peakElement(int arr[], int n)
 {
    // Your code here
-   return pr(arr,0,n-1,n);
+   return peak(arr,n);
 }
