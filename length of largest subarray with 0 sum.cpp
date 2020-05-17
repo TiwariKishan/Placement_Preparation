8ab3c662e5c62e3097f5ae4a3301ae47b6f134b0
Kishan Tiwari <kishan98tiwari@gmail.com>
2020-05-17 05:58:27 +0530

    T.C =O(N) S.C=O(N)

---
+++ length of largest subarray with 0 sum.cpp
@@ -0,0 +1,19 @@
+int maxLen(int A[], int n) {
+    // Your code here
+    int maxle=0,sum=0;
+    map<int,int> prm;
+    for(int i=0;i<n;i++)
+    {   sum+=A[i];
+        if(A[i]==0&&maxle==0)
+        maxle=1;
+        if(sum==0)
+        maxle=i+1;
+        if(prm[.find(sum)]!=prm.end())
+        maxle=max(maxle,i-prm[sum]);
+        else
+        prm[sum]=i;
+   
+    }
+ 
+    return maxle;
+}
