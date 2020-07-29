a03c3edbf1383856472cf59f80c511187e0c4fab
Kishan Tiwari <kishan98tiwari@gmail.com>
2020-07-29 19:11:19 +0530

    Create Stock buy and sell.cpp

---
+++ Stock buy and sell.cpp
@@ -0,0 +1,66 @@
+/*
+  Author - KISHAN TIWARI
+  Dream bigger. Do bigger.
+  Wake up with determination. Go to bed with satisfaction.
+*/
+//Sometimes later becomes never. Do it now.
+#include<bits/stdc++.h>
+using namespace std;
+#define ll           long long
+#define f(i,a,b)     for(ll i=a;i<b;i++)
+#define mod          1000000007
+#define fi           first
+#define se           second
+#define pa           pair <ll,ll>
+#define mp           make_pair
+#define pb           push_back
+#define ve           vector<ll>
+#define vep          vector< pa >
+#define all(a)       a.begin(), a.end()
+#define sz(x)        (ll)x.size()
+#define c(t)         cout<<t<<"\n"
+using namespace std;
+void solve()
+{
+ll n;
+cin>>n;
+ll a[n],fo=0;
+f(i,0,n)
+cin>>a[i];
+ll i=0;
+while(i<n-1)
+{
+    while(i<n-1&&a[i]>=a[i+1])
+        i++;
+    ll buy=i;
+    if(i==n-1)
+        break;
+    i++;
+    while(i<n&&a[i]<=a[i+1])
+        i++;
+        if(i==n)
+            i--;
+    ll sell=i;
+    i++;
+    cout<<"("<<buy<<" "<<sell<<") ";
+    fo=1;
+}
+if(fo==0)
+    cout<<"No Profit";
+cout<<"\n";
+}
+int main()
+{
+    ios::sync_with_stdio(true);
+   //freopen ("input.txt","r",stdin);
+   //freopen ("output.txt","w",stdout);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+return 0;
+}
